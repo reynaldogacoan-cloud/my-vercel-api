5b274079548e2dab1ad64717491c16cd98108c66
reynaldogacoan-cloud <reynaldo.gacoan@gmail.com>
2025-10-16 17:48:26 +0700

    Refactor getConnection function and export it

--- db.js
+++ db.js
@@ -1,15 +1,15 @@
 import mysql from 'mysql2/promise';
 
-export async function getConnection() {
+async function getConnection() {
   const connection = await mysql.createConnection({
     host: process.env.DB_HOST,
     port: process.env.DB_PORT,
     user: process.env.DB_USER,
     password: process.env.DB_PASS,
     database: process.env.DB_NAME,
-    ssl: {
-      rejectUnauthorized: false, // penting untuk Railway
-    },
+    ssl: { rejectUnauthorized: false },
   });
   return connection;
 }
+
+export default getConnection;
